0f1f2c46da493ad0b0650329f36bbce924ce9f4d
Shohin <shohinalimov2008@gmail.com>
2025-01-06 00:11:11 -0500

    chance 2

--- client/src/pages/About.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About: React.FC = () => {
-  return <div>About Page</div>;
-};
-
-export default About;
--- client/src/pages/Account/type.ts
+++
@@ -1,7 +0,0 @@
-export interface AccountDetails {
-  name: string;
-  surname: string;
-  phone: string;
-  email: string;
-  logo: File | null; // Allow both File and null
-}
